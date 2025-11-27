657968254efd89f45b7a0bc873552a456e1f357f
jauzySC <jausyantheswag@gmail.com>
2025-11-27 20:01:43 +0700

    slight adjusment #3 and some other stuff

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ $dbname = "pet_data";
 $conn = new mysqli($serverusername,$username,$password,$dbname);
 
 if($conn->connect_error){
-    die("yo shit failed". $conn->connect_error);
+    die("gagal". $conn->connect_error);
 }
 
 ?>
